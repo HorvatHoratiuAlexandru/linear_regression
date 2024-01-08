ddae5fbdd19cfb1b37953cdf880ba80ec98a17f5
Horvat Horatiu <horvat.horatiu97@gmail.com>
2024-01-08 23:30:43 +0200

    modified the learning rate so it does not shoot to infinity

--- exec.py
+++ exec.py
@@ -1,10 +1,10 @@
 from linear_regression import linear_reg
 import random
 
-def f_x(x_1):
-    return 2 + 5*x_1
+def f_x(x_1, x_2, x_3):
+    return 2 + 5*x_1 - 9*x_2 + 0.5*x_3
 
-obs_x = [[random.randint(1, 65)] for _ in range(150)]
-obs_y = [f_x(obs[0]) for obs in obs_x]
+obs_x = [[random.randint(1, 65), random.randint(1, 65), random.randint(1, 65)] for _ in range(150)]
+obs_y = [f_x(obs[0], obs[1], obs[2]) for obs in obs_x]
 
 print(linear_reg(obs_y, obs_x))
--- linear_regression.py
+++ linear_regression.py
@@ -1,39 +1,34 @@
 import random
-
-
 from typing import List, Dict
 from utils import dot_product
 
-STD_EPOCH = 150
-STD_LEARNING_RATE = 0.1
+STD_EPOCH = 10000
+STD_LEARNING_RATE = 0.0001
 
-def linear_reg(obs_y:List[float], obs_x: List[List[float]], opts: Dict = dict()) -> List[float]:
+def linear_reg(obs_y: List[float], obs_x: List[List[float]], opts: Dict = dict()) -> List[float]:
     epochs = opts.get("epochs") if opts.get("epochs") else STD_EPOCH
     learning_rate = opts.get("learning_rate") if opts.get("learning_rate") else STD_LEARNING_RATE
 
-    num_params = len(obs_x[0]) + 1 #add one for the constant
-    coeficients = [random.uniform(0,1) for _ in range(num_params)]
+    num_params = len(obs_x[0]) + 1  # add one for the constant
+    coeficients = [random.uniform(-0.1, 0.1) for _ in range(num_params)]  # Initialize with small random values
 
     num_obs = len(obs_y)
     for epoch in range(epochs):
-        print("___")
-        gradients = [sqerror_gradient([1] + obs_i, obs_j, coeficients) for obs_i, obs_j in zip(obs_x, obs_y)]
-        grad_means = [sum([g[i] for g in gradients])/num_obs for i in range(len(gradients[0]))]
-        #print(grad_means)
-        #print(coeficients)
-        coeficients = [beta - (g * learning_rate) for g, beta in zip(grad_means, coeficients)]
-        #print(coeficients)
+        for obs_i, obs_j in zip(obs_x, obs_y):
+            gradient = sqerror_gradient([1] + obs_i, obs_j, coeficients)
+            coeficients = [beta - (g * learning_rate) for g, beta in zip(gradient, coeficients)]
+        print(coeficients)
 
     return coeficients
 
 def predict_y(x: List[float], beta: List[float]) -> float:
     assert len(x) == len(beta)
+    result = dot_product(x, beta)
+    return result
 
-    return dot_product(x, beta)
-
-def error(x:List[float], y: float, beta: List[float]) -> float:
+def error(x: List[float], y: float, beta: List[float]) -> float:
     return predict_y(x, beta) - y
 
-def sqerror_gradient(x:List[float], y: float, beta: List[float]):
+def sqerror_gradient(x: List[float], y: float, beta: List[float]) -> List[float]:
     err = error(x, y, beta)
-    return [2*err*x_i for x_i in x] # list of partial derivatives
+    return [2 * err * x_i for x_i in x]  # list of partial derivatives
